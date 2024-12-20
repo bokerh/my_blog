2119ef7992673ba30fb53f249a454c3b1317596b
bokerh <604697000@qq.com>
2024-12-20 15:41:37 +0800

    Update package dependencies to include '@radix-ui/react-scroll-area' for enhanced UI components; modify code-tabs component to integrate scrollable tabs for improved layout and user experience. Enhance test.mdx with additional JavaScript code snippets for better documentation clarity.

--- src/components/code-blocks/code-tabs.tsx
+++ src/components/code-blocks/code-tabs.tsx
@@ -1,7 +1,9 @@
+"use client";
 import { Block, HighlightedCodeBlock, parseProps } from "codehike/blocks";
 import { Pre, HighlightedCode } from "codehike/code";
 import { z } from "zod";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import dynamic from "next/dynamic";
 
 const Schema = Block.extend({ tabs: z.array(HighlightedCodeBlock) });
@@ -21,19 +23,32 @@ export function CodeTabs(props: { tabs: HighlightedCode[] }) {
 
   return (
     <Tabs defaultValue={tabs[0]?.meta} className="dark rounded">
-      <TabsList className="rounded-t-lg rounded-b-none bg-background">
-        {tabs.map((tab) => (
-          <TabsTrigger key={tab.meta} value={tab.meta}>
-            <span className="text-sm text-gray-400 mr-1"> {tab.meta}</span>
-            <CopyButton text={tab.code} />
-          </TabsTrigger>
-        ))}
-      </TabsList>
+      <ScrollArea className="whitespace-nowrap rounded-t-md">
+        <div className="w-full relative h-10">
+          <TabsList className="rounded-t-lg rounded-b-none bg-background flex absolute h-10">
+            {tabs.map((tab) => (
+              <TabsTrigger
+                key={tab.meta}
+                value={tab.meta}
+                className="border-double bg-zinc-700/20"
+              >
+                <span className="text-sm text-gray-400 mr-1"> {tab.meta}</span>
+                <CopyButton text={tab.code} />
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </div>
+        <ScrollBar orientation="horizontal" className="absolute top-[-2px]" />
+      </ScrollArea>
       {tabs.map((tab, i) => (
-        <TabsContent key={tab.meta} value={tab.meta} className="mt-0">
+        <TabsContent
+          key={tab.meta}
+          value={tab.meta}
+          className="mt-0 overflow-x-auto"
+        >
           <Pre
             code={tabs[i]}
-            className="p-4 m-0 rounded-none bg-[#1f1f1f] rounded-b-lg"
+            className="p-4 m-0 rounded-none bg-[#1f1f1f] rounded-b-lg overflow-scroll"
           />
         </TabsContent>
       ))}
